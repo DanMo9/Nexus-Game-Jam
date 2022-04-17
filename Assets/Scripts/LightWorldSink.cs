4f2fd6e1196f5289fe19f50116be156ecd09c9c4
DESKTOP-L2DV6TC\Dan <dtmoore099@gmail.com>
2022-04-16 21:29:41 -0400

    finished stuff

--- Assets/Scripts/LightWorldSink.cs
+++ Assets/Scripts/LightWorldSink.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,6 +12,32 @@ public class LightWorldSink : InteractableObject
             return false;
         
         water.SetActive(true);
+        
+        return true;
+    }
+}
+
+public class LightWorldCrackedMirror : InteractableObject
+{
+    public GameObject mirror;
+    public Sprite fixedMirror;
+    private SpriteRenderer spriteRenderer;
+    private PolygonCollider2D collider;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        collider = GetComponent<PolygonCollider2D>();
+    }
+
+    public override bool Use(Rat rat)
+    {
+        if (!HasEquipped(rat.inventory.Equipped, ToolData.ToolType.Glue))
+            return false;
+        
+        spriteRenderer.sprite = fixedMirror;
+        collider.enabled = false;
+        mirror.SetActive(true);
         
         return true;
     }
